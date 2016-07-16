88d8ceb49708d6c8f65269db8ded60feca0edd94
Rubi Liani <Rubi.liani@gmail.com>
2016-07-16 23:09:11 +0300

    calculate time

--- public/js/controllers/alarm.js
+++ public/js/controllers/alarm.js
@@ -7,6 +7,10 @@ angular.module('SmartAlarm')
     $scope.timeToWakeup = new Date();
     $scope.timeNow = new Date();
     $scope.location = '';
+    $scope.distance='';
+    $scope.distanceValue='';
+    $scope.duration='';
+    $scope.durationValue='';
     $scope.times = ['00:00','00:30','01:00','01:30','02:00','02:30','03:00','03:30','04:00','04:30','05:00','05:30','06:00','06:30',
     '07:00','07:30','08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30',
     '15:00','15:30','16:00','16:30','17:00','18:30','19:00','19:30','20:00','20:30','21:00','21:30','22:00','22:30','23:00','23:30'];
@@ -31,11 +35,52 @@ angular.module('SmartAlarm')
     }
 
     $scope.calculateDistance = function(){
-        geolocation.getDistanceFromPosition($scope.user.hometown,$scope.todayAlarm.location);
+        if(!$scope.todayAlarm.location.data)
+            return;
+        geolocation.getDistanceFromPosition($scope.user.hometown,$scope.todayAlarm.location).then(function(data){
+            $scope.distance = data.rows[0].elements[0].distance.text;
+            $scope.duration = data.rows[0].elements[0].duration.text;
+            $scope.durationValue = data.rows[0].elements[0].duration.value;
+
+        });
+
     }
 
+    $scope.calculateAlarm = function(){
+        var ttg = new Date().setHours(0,0,0,0);
+        var tta = $scope.parseTime($scope.todayAlarm.timeToArrive);
+         console.log(tta);
+        tta.setSeconds(tta.getSeconds()-$scope.durationValue);
+        console.log(tta);
+
+        //ttg.setHours($scope.todayAlarm.timeToArrive)
+    }
+
+    $scope.parseTime = function(timeStr) {
+       
+        var dt = new Date();
+
+        var time = timeStr.match(/(\d+)(?::(\d\d))?\s*(p?)/i);
+        if (!time) {
+                return NaN;
+        }
+        var hours = parseInt(time[1], 10);
+        if (hours == 12 && !time[3]){
+                hours = 12;
+        }
+        else {
+                hours += (hours < 12 && time[3]) ? 12 : 0;
+        }
+
+        dt.setHours(hours);
+        dt.setMinutes(parseInt(time[2], 10) || 0);
+        dt.setSeconds(0, 0);
+        return dt;
+}
+
+
     $scope.saveAlarm = function(){
-        $scope.calculateDistance();
+        
     }
     
     $scope.init();
